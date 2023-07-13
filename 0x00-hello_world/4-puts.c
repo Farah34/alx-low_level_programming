21227b338af5f2cbbd31cd4cdd957a22f269ff1b
Hussein Farah <abtidoon446@gmail.com>
2023-07-13 08:41:46 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints aline with puts function
+ * return:0(Success)
+ */
+int main(void)
+{
+	printf("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
