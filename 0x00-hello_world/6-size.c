73241ad4c30ad53c9fd7a013990bbc9f8c8f0680
Hussein Farah <abtidoon446@gmail.com>
2023-07-25 04:28:44 -0700

    6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,5 +18,6 @@ printf("Size of an int: %lu byte(s)\n", (unsiged long)sizeof(b));
 printf("Size of a long int: %lu byte(s)\n", (unsinged long)sizeof(c));
 printf("Size of a long long int: %lu byte(s)\n", (unsinged long)sizeof(d));
 printf("Size of a float: %lu byte(s)\n", (unsinged long)sizeof(f));
+return (0);
 }
 
