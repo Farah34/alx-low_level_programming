886a5feaa12c9e19d7bda450e0f9b5fbed8f0561
Hussein Farah <abtidoon446@gmail.com>
2023-07-30 03:04:00 -0700

    2-print_alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
- * main - Prints the last digit of a randomly generated number
- *
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-int i;
-for(i=0;i<26;i++)
-{
-putchar(alp[i]);
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
 }
-putchar("\n");
-return (0);
-}
+
